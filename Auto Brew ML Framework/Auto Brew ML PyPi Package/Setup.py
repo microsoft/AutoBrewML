05958d47bc684b9bb2b8cd5e6362256935b77323
Sreeja Deb <srde@microsoft.com>
2023-08-03 12:13:02 +0530

    dependency resolving for AML setup.py file

--- Auto Brew ML Framework/Auto Brew ML PyPi Package/Setup.py
+++ Auto Brew ML Framework/Auto Brew ML PyPi Package/Setup.py
@@ -15,14 +15,15 @@ def readme():
 
 setuptools.setup(
  name='AutoBrewML', 
- version='0.42',
+ version='0.45',
  author="Sreeja Deb",
  author_email="srde@microsoft.com",
  long_description=readme(),
  description="With AutoBrewML Framework the time it takes to get production-ready ML models with great ease and efficiency highly accelerates.",
  packages=setuptools.find_packages(),
  project_urls = project_urls,
- install_requires=['numpy', 'pandas', 'sklearn','pandas_profiling','MarkupSafe==2.0.1','imblearn','scikit-learn==0.23.1','fairlearn','tpot','pyod'],
+ #install_requires=['numpy==1.24.3', 'llvmlite==0.40.1', "numba==0.57.1" ,"scipy==1.11.1" , 'pandas', 'sklearn','pandas_profiling','MarkupSafe==2.0.1','imblearn','scikit-learn==0.23.1','fairlearn','tpot','pyod'],
+ install_requires=['numpy', 'llvmlite', "numba" ,"scipy" , 'pandas', 'sklearn','pandas_profiling','MarkupSafe','imblearn','scikit-learn','fairlearn','tpot','pyod'],
  classifiers=[
  "Programming Language :: Python :: 3",
  "License :: OSI Approved :: MIT License",
